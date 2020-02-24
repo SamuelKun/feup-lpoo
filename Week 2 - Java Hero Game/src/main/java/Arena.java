a1ae9491789408f9aeaee60477b2b912588e823f
Samuel <diogosamuelfernandes@outlook.com>
2020-02-24 18:12:32 +0000

    Class Walls

--- Week 2 - Java Hero Game/src/main/java/Arena.java
+++ Week 2 - Java Hero Game/src/main/java/Arena.java
@@ -5,6 +5,7 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Arena {
@@ -20,12 +21,32 @@ public class Arena {
         this.height = height;
 
         hero = new Hero(10, 10);
+        this.walls = createWalls();
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+
+        return walls;
     }
 
     public void draw(TextGraphics graphics) {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#FFC0CB"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
         hero.draw(graphics);
+
+        for (Wall wall : walls)
+            wall.draw(graphics);
     }
 
     public void setPosition(Position position) {
--- Week 2 - Java Hero Game/src/main/java/Wall.java
+++ Week 2 - Java Hero Game/src/main/java/Wall.java
@@ -1,3 +1,17 @@
-public class Wall {
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 
+public class Wall {
+    private Position position;
+
+    public Wall(int x, int y) {
+        this.position = new Position(x, y);
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FF0000"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.setCharacter(position.getX(), position.getY(), 'X');
+    }
 }
